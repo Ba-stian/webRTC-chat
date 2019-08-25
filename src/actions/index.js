cfb8d22c60de92847eff00aeac2c72c4b88a3e6d
bastian <k.batakovich@gmail.com>
2019-08-25 19:52:12 +0300

    Add basic actions

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const userSubmitted = user => ({
+	type: 'USER_SUBMITTED',
+	payload: user,
+});
+
+export const messageSended = message => ({
+	type: 'MESSAGE_SENDED',
+	payload: message,
+});
+
+export const roomAdded = room => ({
+	type: 'ROOM_ADDED',
+	payload: room,
+});
