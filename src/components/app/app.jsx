5a32b08f1c61878bd4244898fe839dcfa325e4a2
bastian <k.batakovich@gmail.com>
2019-08-31 14:22:43 +0300

    Init socket in root component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import Chat from '../chat';
+import io from 'socket.io-client';
 import app from './app.css';
+import Chat from '../chat';
 import User from '../user';
 
 
 class App extends Component {
 	state = {
 		user: '',
+		socket: '',
 	};
+
+	componentDidMount() {
+		this.initSocket();
+	}
+
+	initSocket = () => {
+		const socket = io('127.0.0.1:8000');
+		this.setState({
+			socket,
+		});
+	};
+
 
 	onUserSubmit = (username) => {
 		this.setState({
 			user: username,
 		});
+		this.addUser();
+	};
+
+	addUser = () => {
+		const { socket, user } = this.state;
+		socket.emit('USER_CONNECTED', user);
 	};
 
 	onLogout = () => {
@@ -22,7 +42,7 @@ class App extends Component {
 	};
 
 	render() {
-		const { user } = this.state;
+		const { user, socket } = this.state;
 		if (!user) {
 			return (
 				<div className={app.app}>
@@ -32,7 +52,7 @@ class App extends Component {
 		}
 		return (
 			<div className={app.app}>
-				<Chat user={user} onLogout={this.onLogout} />
+				<Chat user={user} onLogout={this.onLogout} socket={socket} />
 			</div>
 		);
 	}
